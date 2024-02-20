47b2e86a9459d1659229943becaddbe99d2e527f
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-02-20 13:38:20 -0500

    get tour by ID

--- app.js
+++ app.js
@@ -27,6 +27,31 @@ app.get('/api/v1/tours', (req, res) => {
     })
 })
 
+//GET tour by id
+app.get('/api/v1/tours/:id', (req, res) => {
+
+    const id = req.params.id * 1 //convert string to a number
+    const tour = tours.find(el => el.id === id)
+    //error handling
+    // if(id > tours.length) {
+    if(!tour){
+        return  res.status(404).json({
+                status: 'fail',
+                message: `Tour with id ${id} not found.`
+             });
+    }
+
+    
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+        
+    })
+})
+
 //handle POST request to add new tour
 app.post('/api/v1/tours', (req, res) => {
     const newId = tours[tours.length - 1].id + 1
