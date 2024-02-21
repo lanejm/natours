f9584209cd4c9294543d970411c164c9df01e668
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-02-21 11:41:50 -0500

    added morgan

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan")
 const app = express();
 
+//1) MIDDLEWARES
+app.use(morgan('dev'));
 app.use(express.json()); //middleware
 
 app.use((req, res, next) => {
@@ -17,6 +20,7 @@ const tours = JSON.parse(
   fs.readFileSync("./starter/dev-data/data/tours-simple.json")
 );
 
+//2) ROUTE HANDLERS
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
@@ -115,6 +119,7 @@ const deleteTour = (req, res) => {
 //DELETE requests - just and example, doesn't really delete tour
 // app.delete('/api/v1/tours/:id', deleteTour );
 
+//3) ROUTES
 app.route("/api/v1/tours").get(getAllTours).post(createTour);
 app
   .route("/api/v1/tours/:id")
@@ -122,6 +127,8 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+
+//4) START THE SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
