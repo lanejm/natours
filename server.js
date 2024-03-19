1d8fcacfbbb5d1fb2e95f407e651397c6861ae85
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-03-19 12:19:10 -0400

    model schema

--- server.js
+++ server.js
@@ -6,14 +6,35 @@ dontenv.config({ path: './config.env' })
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
-mongoose.connect(DB, {
+mongoose
+//.connect(process.env.DATABASE_LOCAL) connect to local DB
+.connect(DB, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
-}).then(con => {
-  console.log(con.connections)
+}).then(() => {
   console.log('DB connection successful')
 })
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  }
+})
+
+const Tour = mongoose.model( 'Tour', tourSchema )
+
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
