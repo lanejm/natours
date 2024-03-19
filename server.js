2ae19c112fd0545065c1a6ab82c86ca9eeebbd12
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-03-19 11:33:52 -0400

    mongoDB connection

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+const mongoose = require('mongoose')
+const dontenv = require('dotenv')
 const app = require("./app")
 
+dontenv.config({ path: './config.env' })
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(con => {
+  console.log(con.connections)
+  console.log('DB connection successful')
+})
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
