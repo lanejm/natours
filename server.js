e8be1108a1a20bdf11225431393abf09f9652d1a
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-03-19 12:28:51 -0400

    document creation using mongoDB

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: true
+    unique: true //impossible to have two tours with same name
   },
   rating: {
     type: Number,
@@ -34,6 +34,18 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model( 'Tour', tourSchema )
 
+const testTour = new Tour({
+  name: "The Park Camper",
+  rating: 4.7,
+  price: 997
+})
+
+//saves document to the DB
+testTour.save().then(doc => {
+console.log(doc)
+}).catch(err => {
+  console.log("ERROR, ERROR, ERROR", err)
+}) 
 
 const port = 3000;
 app.listen(port, () => {
