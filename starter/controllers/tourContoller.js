4616f255ed5ef0d3d2466d6475c159bb3cbb2d6f
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-05-28 15:17:04 -0400

    pagination

--- starter/controllers/tourContoller.js
+++ starter/controllers/tourContoller.js
@@ -60,6 +60,17 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v'); //select all fields except __v
     }
 
+    //4) Pagination
+    const page = req.query.page * 1 || 1; //if page is not in query it will default to page 1
+    const limit = req.query.limit * 1 || 100
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit)
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     //execute query
     const tours = await query;
     //send response
